9f7e02d17d45aad8cf39df2b660102c3c31e7cd5
carlotalobo <carlota.lobo.sanz@gmail.com>
2021-05-11 10:10:36 +0200

    **

---
+++ MvcCoreAWSBlank/Controllers/PersonajesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MvcCoreAWSBlank.Models;
+using MvcCoreAWSBlank.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreAWSBlank.Controllers
+{
+    public class PersonajesController : Controller
+    {
+        PersonajesServiceDynamodb servicedynamo;
+        PersonajesServiceS3 services3;
+
+        public PersonajesController(PersonajesServiceDynamodb servicedynamo, PersonajesServiceS3 services3)
+        {
+            this.servicedynamo = servicedynamo;
+            this.services3 = services3;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await this.servicedynamo.GetPersonajes());
+        }
+        public async IActionResult CreatePersonaje()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreatePersonaje(IFormFile imagen, Personaje personaje )
+        {
+            await this.servicedynamo.CreatePersonaje()
+        }
+    }
+}
--- MvcCoreAWSBlank/Services/PersonajesServiceDynamodb.cs
+++ MvcCoreAWSBlank/Services/PersonajesServiceDynamodb.cs
@@ -1,5 +1,9 @@
-﻿using Amazon.S3;
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.S3;
 using Microsoft.Extensions.Configuration;
+using MvcCoreAWSBlank.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,8 +11,35 @@ using System.Threading.Tasks;
 
 namespace MvcCoreAWSBlank.Services
 {
+   
     public class PersonajesServiceDynamodb
     {
-        
+        private DynamoDBContext context; 
+
+        public PersonajesServiceDynamodb()
+        {
+            AmazonDynamoDBClient client = new AmazonDynamoDBClient();
+            this.context = new DynamoDBContext(client);
+        }
+
+        public async Task CreatePersonaje(Personaje p)
+        {
+            await this.context.SaveAsync<Personaje>(p);
+        }
+
+        public async Task<List<Personaje>> GetPersonajes()
+        {
+            var tabla = this.context.GetTargetTable<Personaje>();
+            var scanoptions = new ScanOperationConfig();
+            var result = tabla.Scan(scanoptions);
+            List<Document> data = await result.GetNextSetAsync();
+            IEnumerable<Personaje> personajes = this.context.FromDocuments<Personaje>(data);
+            return personajes.ToList();
+        }
+
+        public async Task<Personaje> GetPersonaje(int idpersonaje)
+        {
+            return await this.context.LoadAsync<Personaje>(idpersonaje);
+        }
     }
 }
